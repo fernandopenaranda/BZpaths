3c4c63560a4d5aaf905ad5525bd01d2ba67fa82a
fernandopenaranda <49433311+fernandopenaranda@users.noreply.github.com>
2025-11-03 19:05:42 +0100

    Delete BZpaths.jl

--- BZpaths.jl
+++
@@ -1,8 +0,0 @@
-module BZpaths
-    using Brillouin
-    using LinearAlgebra
-    using Bravais
-    using CairoMakie
-    include("kpaths.jl")
-    export custom_path, kpath, plot_kpath, plot_observable_in_kpath
-end
